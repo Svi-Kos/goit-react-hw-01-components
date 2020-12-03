7aa869f01658210dcab47058e29bded0dba933bd
Svi-Kos <svitlanakos@gmail.com>
2020-12-03 12:42:09 +0200

    правки2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 // import logo from './logo.svg';
 import Profile from 'components/Profile/ProfileCard/Profile';
-import StatisticList from 'components/Statistics/StatList/StatisticList';
-import SectionStatistics from 'components/Statistics/StatSection/SectionStatistics';
-import FriendList from 'components/Friends/FriendList/FriendList';
-import TransactionList from 'components/TransactionHistory/TransactionList/TransactionList';
-import SectionTransactionHistory from 'components/TransactionHistory/TransactionSection/SectionTransactionHistory';
-import user from 'components/Profile/dataUser/user.json';
-import statData from 'components/Statistics/dataStatistic/statistical-data.json';
-import friends from 'components/Friends/dataFriends/friends.json';
-import transactions from 'components/TransactionHistory/dataTransaction/transactions.json';
+import StatisticList from 'components/Statistics/StatList/StatisticList.jsx';
+import SectionStatistics from 'components/Statistics/StatSection/SectionStatistics.jsx';
+import FriendList from 'components/Friends/FriendList/FriendList.jsx';
+import TransactionList from 'components/TransactionHistory/TransactionList/TransactionList.jsx';
+import SectionTransactionHistory from 'components/TransactionHistory/TransactionSection/SectionTransactionHistory.jsx';
+import user from 'data/user.json';
+import statData from 'data/statistical-data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 import './App.css';
 
 function App() {
